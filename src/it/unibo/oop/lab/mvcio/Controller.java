291b97c9630e451f3cb2912bb9347494b6a3e011
saribricka <saribricka44@gmail.com>
2020-12-23 12:47:55 +0100

    MVCIO and 2

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,5 +1,14 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import org.apache.commons.io.FileUtils;
+
 /**
  * 
  */
@@ -28,4 +37,39 @@ public class Controller {
      * to a software that runs correctly on every platform.
      */
 
-}
+    private String currentFilePath = System.getProperty("user.home") + System.getProperty("file.separator") + "output.txt";
+    private File currentFile = new File(this.currentFilePath);
+
+    public final File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public final void setCurrentFile(final File newFile) {
+        Objects.requireNonNull(newFile);
+        this.currentFile = newFile;
+        this.currentFilePath = newFile.getAbsolutePath();   //What's the diff between getPath and getAbsolutePath
+    }
+
+    public final String getCurrentFilePath() {
+        return this.currentFile.getPath();
+    }
+
+    public final void setCurrentFilePath(final String newFilePath) {
+        Objects.requireNonNull(newFilePath);
+        final File newFile = new File(newFilePath);
+        if (!newFile.isFile()) {
+            throw new IllegalArgumentException(newFile.getAbsolutePath() + "cannot be used as a file");
+        }
+        this.currentFile = newFile;
+        this.currentFilePath = newFilePath;
+    }
+
+    /**
+     * 
+     * @param textFile
+     * @throws IOException
+     */
+    public final void stringFile(final String textFile) throws IOException {
+            FileUtils.write(this.currentFile, textFile, StandardCharsets.UTF_8);
+    }
+} 
--- src/it/unibo/oop/lab/mvcio/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvcio/SimpleGUI.java
@@ -1,17 +1,20 @@
 package it.unibo.oop.lab.mvcio;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
-
     private final JFrame frame = new JFrame();
+    private final Controller myController;
+    private final JPanel contentPanel = new JPanel();
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -36,7 +39,12 @@ public final class SimpleGUI {
     /**
      * builds a new {@link SimpleGUI}.
      */
-    public SimpleGUI() {
+    /**
+     * 
+     * @param myController
+     */
+    public SimpleGUI(final Controller myController) {
+        this.myController = myController;
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -50,13 +58,53 @@ public final class SimpleGUI {
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         final int sw = (int) screen.getWidth();
         final int sh = (int) screen.getHeight();
-        frame.setSize(sw / 2, sh / 2);
+        this.frame.setSize(sw / 2, sh / 2);
         /*
          * Instead of appearing at (0,0), upper left corner of the screen, this
          * flag makes the OS window manager take care of the default positioning
          * on screen. Results may vary, but it is generally the best choice.
          */
-        frame.setLocationByPlatform(true);
+        this.frame.setLocationByPlatform(true);
+        /*
+         * SetDefaultCloseOperation
+         */
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * Graphical interface
+         */
+        this.contentPanel.setLayout(new BorderLayout());    //se inizializzo qui il JPanel, non devo usare this
+        final JTextArea textArea = new JTextArea("Scrivi qui..");
+        final JButton save = new JButton("SAVE");
+        this.contentPanel.add(textArea, BorderLayout.CENTER);
+        this.contentPanel.add(save, BorderLayout.SOUTH);
+
+        this.frame.getContentPane().add(contentPanel, BorderLayout.CENTER);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    SimpleGUI.this.myController.stringFile(textArea.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(SimpleGUI.this.frame, e1, "FILE ERROR:", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace();
+                }
+                System.out.println("Visualizzo file: " + textArea.getText());
+            }
+        });
+
+//        DocumentListener myListener = new DocumentListener();
+//        textArea.getDocument().addDocumentListener(myListener);
+
+        this.frame.setVisible(true);
     }
 
+    public JPanel getPane() {
+        return this.contentPanel;
+    }
+
+    public static void main(final String[] args) {
+        new SimpleGUI(new Controller());
+    }
 }
+
